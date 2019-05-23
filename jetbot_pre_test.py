032f734c52ad07185e9628032255a7d6b2f263f2
MoneyFun <1483586698@qq.com>
2019-05-23 10:36:50 +0800

    gluon.data

--- jetbot_pre_test.py
+++ jetbot_pre_test.py
@@ -1,6 +1,5 @@
 import mxnet
 from collections import namedtuple
-import numpy as np
 import cv2
 
 Batch = namedtuple('Batch', ['data'])
@@ -10,33 +9,16 @@ model = mxnet.mod.Module(symbol=sym, label_names=['data'])
 model.bind(for_training=False, data_shapes=[('data', (1, 3, 224, 224))])
 model.set_params(arg_params, aux_params, allow_missing=True)
 
-img = cv2.imread("/home/luke/Download/face/d2l-zh/test_data/not-hotdog/0.png")
-pic = cv2.resize(img, (256, 256), interpolation=cv2.INTER_LINEAR)
-start_pos = int((256 - 224)/2)
-end_pos = int(start_pos + 224)
-cropImg = pic[start_pos:end_pos, start_pos:end_pos]
-# cv2.imshow("cropImg", cropImg)
-# cv2.waitKey(10000)
-normalize_pos = cropImg/255
-normalize_pos = normalize_pos[:,:,::-1]
-print(normalize_pos.shape)
-# print(normalize_pos[0])
-bgr_mean = np.array([0.485, 0.456, 0.406])
-mean_pic = normalize_pos - bgr_mean
-print(mean_pic.shape)
-# print(mean_pic)
-bgr_std = np.array([0.229, 0.224, 0.225])
-std_pic = mean_pic/bgr_std
-print(std_pic.shape)
-final_pic = np.zeros((3, 224, 224))
-final_pic[0] = std_pic[:,:,0]
-final_pic[1] = std_pic[:,:,1]
-final_pic[2] = std_pic[:,:,2]
-# print(final_pic)
+img = cv2.imread("/home/luke/Download/face/data/hotdog/train/not-hotdog/9.png")
+img = img[:, :, ::-1]  # bgr_to_rgb
 
-#
-expand_pic = np.expand_dims(final_pic, axis=0)
-# print(expand_pic.shape)
-model.forward(Batch([mxnet.nd.array(expand_pic)]))
+img = mxnet.nd.array(img, dtype="uint8")
+img = mxnet.gluon.data.vision.transforms.Resize(256).forward(img)
+img = mxnet.gluon.data.vision.transforms.CenterCrop(224).forward(img)
+img = mxnet.gluon.data.vision.transforms.ToTensor().forward(img)
+img = mxnet.gluon.data.vision.transforms.Normalize(
+    [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]).forward(img)
+
+model.forward(Batch([mxnet.nd.expand_dims(img, axis=0)]))
 prob = model.get_outputs()[0].asnumpy()
 print(prob)
