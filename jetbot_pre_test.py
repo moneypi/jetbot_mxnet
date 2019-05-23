2aeb6e18b473a4916d6464655a1cc6528db34613
MoneyFun <1483586698@qq.com>
2019-05-23 14:10:55 +0800

    SymbolBlock

--- jetbot_pre_test.py
+++ jetbot_pre_test.py
@@ -2,13 +2,6 @@ import mxnet
 from collections import namedtuple
 import cv2
 
-Batch = namedtuple('Batch', ['data'])
-
-sym, arg_params, aux_params = mxnet.model.load_checkpoint("jetbot_pre", 0)
-model = mxnet.mod.Module(symbol=sym, label_names=['data'])
-model.bind(for_training=False, data_shapes=[('data', (1, 3, 224, 224))])
-model.set_params(arg_params, aux_params, allow_missing=True)
-
 img = cv2.imread("/home/luke/Download/face/data/hotdog/train/not-hotdog/9.png")
 img = img[:, :, ::-1]  # bgr_to_rgb
 
@@ -18,7 +11,11 @@ img = mxnet.gluon.data.vision.transforms.CenterCrop(224).forward(img)
 img = mxnet.gluon.data.vision.transforms.ToTensor().forward(img)
 img = mxnet.gluon.data.vision.transforms.Normalize(
     [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]).forward(img)
+# print(img[0,0,:])
 
-model.forward(Batch([mxnet.nd.expand_dims(img, axis=0)]))
-prob = model.get_outputs()[0].asnumpy()
-print(prob)
+sym = mxnet.sym.load('jetbot_pre-symbol.json')
+data = mxnet.sym.var('data')
+net = mxnet.gluon.SymbolBlock(sym, data)
+net.load_parameters('jetbot_pre-0000.params')
+res = net(mxnet.nd.expand_dims(img, axis=0))
+print(res)
